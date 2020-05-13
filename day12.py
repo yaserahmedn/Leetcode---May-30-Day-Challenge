2276c4be284924ba8f7cc20058e5c596ccfd7e7f
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-05-13 14:33:53 +0530

    Update day12.py

--- day12.py
+++ day12.py
@@ -12,10 +12,39 @@
 # Output: 10
  
 
-
+#O(N) solution
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         answer=0
         for num in nums:
             answer=answer^num
+        return answer
+
+#O(logN) solution - Source geeksforgeeks
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        def binarySearch(left,right):
+            if left>right:
+                return None
+            if left==right:
+                return nums[left]
+            
+            mid=(left+right)//2
+            
+            if mid%2 == 0: 
+
+                if nums[mid] == nums[mid+1]: 
+                    return binarySearch(mid+2, right) 
+                else: 
+                    return binarySearch(left, mid) 
+
+            else: 
+                 
+                if nums[mid] == nums[mid-1]: 
+                    return binarySearch(mid+1, right) 
+                else: 
+                    return binarySearch(left, mid-1) 
+        
+        
+        answer=binarySearch(0,len(nums)-1)
         return answer
