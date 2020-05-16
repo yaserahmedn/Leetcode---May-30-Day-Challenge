09ca927045470693490943964cf18454bc11f554
Yaser Ahmed N <41563530+yaserahmedn@users.noreply.github.com>
2020-05-16 13:18:17 +0530

    Create day16.py

---
+++ day16.py
@@ -0,0 +1,49 @@
+#Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+#You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+#Example 1:
+
+#Input: 1->2->3->4->5->NULL
+#Output: 1->3->5->2->4->NULL
+#Example 2:
+
+#Input: 2->1->3->5->6->4->7->NULL
+#Output: 2->3->6->7->1->5->4->NULL
+#Note:
+
+#The relative order inside both the even and odd groups should remain as it was in the input.
+#The first node is considered odd, the second node even and so on ...
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head or not head.next or not head.next.next:
+            return head
+        
+        odd=head
+        even=head.next
+        
+        temp=even
+        
+        while odd.next and even.next:
+            odd.next=even.next
+            
+            if odd.next.next !=None:
+                even.next=odd.next.next
+            else:
+                even.next=None
+
+            
+            if odd.next!=None:
+                odd=odd.next
+            
+            if even.next!=None:
+                even=even.next
+                
+        odd.next=temp
+        return head
